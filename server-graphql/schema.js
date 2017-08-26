c1faa927525678f6b3f5739e8aba2b0f494aea71
Lorgio Trinidad <lorgio.trinidad@globant.com>
2017-08-26 19:36:31 -0300

    Add schemas

--- server-graphql/schema.js
+++ server-graphql/schema.js
@@ -1,12 +1,40 @@
 const { makeExecutableSchema } = require('graphql-tools');
 const typeDefs = `
+    # Esto es un curso
     type Curso {
         id: ID!
         titulo: String!
+        # Esta es la descripcion del curso
+        descripcion: String!
+        profesor: Profesor
+        rating: Float
+        comentarios: [Comentario]
+    }
+
+    type Profesor {
+        id: ID!
+        nombre: String!
+        nacionalidad: String!
+        genero: Genero
+        cursos: [Curso]
+    }
+
+    enum Genero {
+        MASCULINO
+        FEMENINO
+    }
+
+    type Comentario {
+        id: ID!
+        nombre: String!
+        cuerpo: String!
     }
 
     type Query {
         cursos: [Curso]
+        profesores: [Profesor]
+        curso(id: Int): Curso
+        profesor(id: Int): Profesor
     }
 `
 
